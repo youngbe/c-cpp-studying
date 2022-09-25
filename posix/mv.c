7586c46b3b843b4e5d93c5f30b2f862bd6b88101
youngbe <24429886+youngbe@users.noreply.github.com>
2022-09-25 17:14:29 +0800

    Create mv.c

---
+++ posix/mv.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        fputs("argc != 3\n", stderr);
+        return -1;
+    }
+    printf("%d\n", rename(argv[1], argv[2]));
+    return 0;
+}
