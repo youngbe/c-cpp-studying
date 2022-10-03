574debb14c8bc8ba1b0262006e507a7d6ae667b8
youngbe <24429886+youngbe@users.noreply.github.com>
2022-10-03 23:42:44 +0800

    Update and rename fork_exec.c to fork_exec_wait.c

---
+++ posix/fork_exec_wait.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define fork_execlp_wait(file, ...) \
+({ \
+    int _ret; \
+    pid_t pid = fork(); \
+    if (pid == -1) { \
+        fputs("error: fork failed!\n", stderr); \
+        _ret = -1; \
+        goto __label_return; \
+    } \
+    if (pid == 0) { \
+        _ret = execlp(file, __VA_ARGS__); \
+        if (_ret == 0) { \
+            fputs("error: execlp return 0!\n", stderr); \
+            _ret = -1; \
+        } \
+        exit(_ret); \
+    } \
+    wait(&_ret); \
+__label_return: \
+    _ret; \
+})
+
+int fork_execve_wait(const char *const pathname, char *const argv[], char *const envp[])
+{
+    pid_t pid = fork();
+    if (pid == -1) {
+        fputs("error: fork failed!\n", stderr);
+        return -1;
+    }
+    if (pid == 0) {
+        int ret = execve(pathname, argv, envp);
+        if (ret == 0) {
+            fputs("error: execve return 0!\n", stderr);
+            ret = -1;
+        }
+        exit(ret);
+    }
+    int statval;
+    wait(&statval);
+    return statval;
+}
+
+
+int main(int argc, char *argv[])
+{
+    char *env = NULL;
+    int statval = fork_execve_wait(argv[1], &argv[1], &env);
+    if (WIFEXITED(statval) && WEXITSTATUS(statval) == 0)
+        printf("子进程执行成功！%d\n", statval);
+    else
+        printf("子进程执行失败！%d\n", statval);
+
+    statval = fork_execlp_wait("ls", "ls", "-al", "/", NULL);
+    if (WIFEXITED(statval) && WEXITSTATUS(statval) == 0)
+        printf("子进程执行成功！%d\n", statval);
+    else
+        printf("子进程执行失败！%d\n", statval);
+    return 0;
+}
