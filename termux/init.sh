e2898cec15f690eccb4218d42e0f5e71f1cf87b0
youngbe <24429886+youngbe@users.noreply.github.com>
2022-11-06 04:06:40 +0800

    Update init.sh

--- termux/init.sh
+++ termux/init.sh
@@ -6,7 +6,7 @@ gcc \\
     -fstack-reuse=all -freg-struct-return -fipa-pta -fdevirtualize-at-ltrans -fmerge-all-constants -funroll-all-loops -floop-nest-optimize \\
     -fno-exceptions -fno-asynchronous-unwind-tables -fno-unwind-tables \\
     -fstack-check=no -fno-stack-clash-protection -fno-stack-protector -fno-split-stack -fcf-protection=none -fno-sanitize=all -fno-instrument-functions \\
-    -std=gnu2x -D_GNU_SOURCE -g0 -Ofast -Wall -Wextra "\$@"
+    -std=gnu2x -D_GNU_SOURCE -g0 -Ofast -Wall -Wextra -Wstrict-prototypes "\$@"
 EOF
 
 cat > /bin/g++-me << EOF
@@ -17,7 +17,7 @@ g++ \\
     -fstack-reuse=all -freg-struct-return -fipa-pta -fdevirtualize-at-ltrans -fmerge-all-constants -funroll-all-loops -floop-nest-optimize \\
     -fno-rtti \\
     -fstack-check=no -fno-stack-clash-protection -fno-stack-protector -fno-split-stack -fcf-protection=none -fno-sanitize=all -fno-instrument-functions -fvtable-verify=none -fno-threadsafe-statics \\
-    -std=gnu++23 -D_GNU_SOURCE -g0 -Ofast -Wall -Wextra "\$@"
+    -std=gnu++23 -D_GNU_SOURCE -g0 -Ofast -Wall -Wextra -Wstrict-prototypes "\$@"
 EOF
 
 chmod +x /bin/gcc-me /bin/g++-me
