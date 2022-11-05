bc4e8a67a80af25e9d964aa42a91b30ceda6f118
youngbe <24429886+youngbe@users.noreply.github.com>
2022-11-05 16:57:41 +0800

    Create signal_nanosleep.c

---
+++ posix/signal_nanosleep.c
@@ -0,0 +1,28 @@
+#include <signal.h>
+
+#include <time.h>
+#include <stdio.h>
+
+void sighandler(int sig)
+{
+    printf("get sig:%d\n", sig);
+}
+
+int main()
+{
+    // ctrl + '\'
+    signal(SIGQUIT, sighandler);
+    // kill
+    signal(SIGTERM, sighandler);
+    // ctrl + z
+    signal(SIGTSTP, sighandler);
+    // ctrl + c
+    //signal(SIGINT, sighandler);
+    printf("1111\n");
+    struct timespec ts;
+    ts.tv_sec = 1;
+    ts.tv_nsec = 99999999;
+    printf("nanosleep return :%d\n", nanosleep(&ts, NULL));
+    printf("2222\n");
+    return 0;
+}
