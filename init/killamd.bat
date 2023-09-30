360e5aac32db6fd9d5b538fae6d82863c09725d0
youngbe <24429886+youngbe@users.noreply.github.com>
2023-09-30 22:39:17 +0800

    Add files via upload

---
+++ init/killamd.bat
@@ -0,0 +1,12 @@
+taskkill /f /t /IM RadeonSoftware.exe
+taskkill /f /t /IM amdfendrsr.exe
+taskkill /f /t /IM atieclxx.exe
+taskkill /f /t /IM atiesrxx.exe
+taskkill /f /t /IM atiesrxx.exe
+taskkill /f /t /IM AMDNoiseSuppression.exe
+taskkill /f /t /IM amdow.exe
+taskkill /f /t /IM AMDRSServ.exe
+taskkill /f /t /IM CPUMetricsServer.exe
+taskkill /f /t /IM AMDRSSrcExt.exe
+taskkill /f /t /IM cncmd.exe
+taskkill /f /t /IM AMDGameConfigServ.exe
