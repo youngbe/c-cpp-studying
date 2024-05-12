e4d0949c6314bf46712391786c147e5dced44c5f
youngbe <24429886+youngbe@users.noreply.github.com>
2024-05-12 15:24:14 +0800

    Update gcc-me.sh

--- init/gcc-me.sh
+++ init/gcc-me.sh
@@ -2,8 +2,16 @@
 
 args=(
 # Options for Code Generation Conventions
--fstack-reuse=all -fno-unwind-tables -fno-asynchronous-unwind-tables -fno-exceptions -fjump-tables -fbit-tests -fno-strict-volatile-bitfields
+-fstack-reuse=all
+-fno-unwind-tables
+-fno-asynchronous-unwind-tables
+-fno-exceptions
+-fjump-tables
+-fbit-tests
+-fno-strict-volatile-bitfields
 -fdelete-dead-exceptions
+
+
 
 # Program Instrumentation Options
 -fno-sanitize=all
@@ -21,33 +29,62 @@ args=(
 # -fvtable-verify=none // C++ only
 # -fno-instrument-functions
 
+
+
 # default optimize
 -fstdarg-opt
 -ftrivial-auto-var-init=uninitialized
 -fno-conserve-stack
-# -fdelete-null-pointer-checks
+
+
 
 # O3
--fomit-frame-pointer -fstrict-aliasing -foptimize-sibling-calls -ftree-loop-vectorize -ftree-slp-vectorize
+-fomit-frame-pointer
+-fstrict-aliasing
+-foptimize-sibling-calls
+-ftree-loop-vectorize
+-ftree-slp-vectorize
+# -fdelete-null-pointer-checks
+
+
 
 # Ofast:
-# -ffast-math
+# -ffast-math: 在后面打开
 -fallow-store-data-races
 # 仅对共享库有效
 -fno-semantic-interposition
 
+
+
 # Ofast之外：
--fipa-pta -fdevirtualize-at-ltrans -fmerge-all-constants -funroll-loops -funroll-all-loops
+-fipa-pta
+-fdevirtualize-at-ltrans
+-fmerge-all-constants
+-funroll-loops
+-funroll-all-loops
+
+
 
 # math相关
 -fno-float-store
 -ffp-contract=fast
 -ffast-math
 # fast-math enable:
--fno-math-errno -funsafe-math-optimizations -ffinite-math-only -fno-rounding-math -fno-signaling-nans -fcx-limited-range -fexcess-precision=fast
+-fno-math-errno
+# -funsafe-math-optimizations: 链接器也需要这个选项，会链接到crtfastmath.o，设置mxcsr
+-funsafe-math-optimizations
+-ffinite-math-only
+-fno-rounding-math
+-fno-signaling-nans
+-fcx-limited-range
+-fexcess-precision=fast
 # unsafe-math-optimizations enables:
--fno-signed-zeros -fno-trapping-math -fassociative-math -freciprocal-math
-# -funsafe-math-optimizations 链接器也需要这个选项，会链接到crtfastmath.o，设置mxcsr
+-fno-signed-zeros
+-fno-trapping-math
+-fassociative-math
+-freciprocal-math
+
+
 
 # other
 -std=gnu23 -D_GNU_SOURCE -g0 -Ofast -Wall -Wextra -Wshadow
