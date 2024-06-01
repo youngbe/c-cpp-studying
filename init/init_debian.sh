62e30bb3f91aab27eecb24048a08c2091c7decee
youngbe <24429886+youngbe@users.noreply.github.com>
2024-06-02 01:04:20 +0800

    Update init_debian.sh

--- init/init_debian.sh
+++ init/init_debian.sh
@@ -1,201 +1,59 @@
 apt update
 
-llvm_version="-17"
+clang_version="18"
+gcc_version="14"
 
 # 常用操作
-apt --no-install-recommends -y install git git-lfs openssh-client wget curl ca-certificates vim tar gzip xz-utils bzip2 lzma cpio apt-file dpkg-dev iputils-ping bind9-dnsutils
+apt --no-install-recommends -y install git git-lfs openssh-client wget curl ca-certificates vim tar gzip xz-utils bzip2 lzma cpio apt-file dpkg-dev iputils-ping bind9-dnsutils make gnome-text-editor diff patch gdb gdb-multiarch
 apt-file update
 
 # bcompare
-# fonts-noto-cjk-extra fonts-noto-cjk:中文GUI字体显示
+# 中文GUI字体显示
 apt --no-install-recommends -y install fonts-noto-cjk-extra fonts-noto-cjk shared-mime-info
-[ "$EUID" == 0 ] && export QT_GRAPHICSSYSTEM=native
-echo 'export QT_GRAPHICSSYSTEM=native' >> "/root/.profile"
 
 # c/c++
-#echo 'deb http://apt.llvm.org/lunar/ llvm-toolchain-lunar-17 main' >> /etc/apt/sources.list
-#echo 'deb-src http://apt.llvm.org/lunar/ llvm-toolchain-lunar-17 main' >> /etc/apt/sources.list
-#curl -L https://apt.llvm.org/llvm-snapshot.gpg.key | tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
-#apt update
-apt --no-install-recommends -y install gcc g++ libc6-dev "clang${llvm_version}" "lld${llvm_version}" "libclang-rt${llvm_version}-dev" "libunwind${llvm_version}-dev" "libc++${llvm_version}-dev" "libc++abi${llvm_version}-dev" gdb
-# c/c++:i386
-apt --no-install-recommends -y install libc6-dev-i386 lib32gcc-13-dev
-# or 
-apt --no-install-recommends -y install libc6-dev-amd64-cross libc6-dev-i386-amd64-cross lib32gcc-13-dev
-# or
-apt --no-install-recommends -y install libc6-dev:i386 lib32gcc-13-dev
-# or
-apt --no-install-recommends -y install gcc-i686-linux-gnu libc6-dev-i386-cross
+apt --no-install-recommends -y install gcc-${gcc_version} g++-${gcc_version} libc6-dev clang-${clang_version} lld-${clang_version}
+# c/c++: i386
+apt --no-install-recommends -y install libc6-dev-i386 lib32gcc-${gcc_version}-dev
 # c/c++: arm64
-dpkg --add-architecture arm64
-apt update
-apt --no-install-recommends -y install gcc-aarch64-linux-gnu libc6-dev-arm64-cross g++-aarch64-linux-gnu "libclang-rt${llvm_version}-dev:arm64"
-# c/c++: arm32
-apt --no-install-recommends -y install gcc-arm-linux-gnueabi libc6-dev-armel-cross
+apt --no-install-recommends -y install gcc-${gcc_version}-aarch64-linux-gnu libc6-dev-arm64-cross g++-${gcc_version}-aarch64-linux-gnu
 # c/c++: armhf
-apt --no-install-recommends -y install gcc-arm-linux-gnueabihf libc6-dev-armhf-cross
+apt --no-install-recommends -y install gcc-${gcc_version}-arm-linux-gnueabihf libc6-dev-armhf-cross g++-${gcc_version}-arm-linux-gnueabihf
 # 内核编译
-apt --no-install-recommends -y install "clang${llvm_version}" "lld${llvm_version}" "llvm${llvm_version}" make bc flex bison python3-minimal libelf-dev libssl-dev libncurses-dev dwarves
+apt --no-install-recommends -y install llvm-${clang_version} make bc flex bison python3-minimal libelf-dev libssl-dev libncurses-dev dwarves
 
 
-
-
-
-# -fdwarf2-cfi-asm -fplt :只是配置
-# -floop-nest-optimize ：此选项已经存在多年，但仍未启用
-# -fsection-anchors ： x86 不支持
-# -mskip-rax-setup ：只支持x86_64 -mgeneral-regs-only
-# -funroll-completely-grow-size ：目前没有发现作用
-# -fstrict-volatile-bitfields ：很有意思
-# -frecord-gcc-switches
-# -finhibit-size-directive
-# -ftree-vectorize ：本身没作用，显式打开相当于打开 -ftree-loop-vectorize -ftree-slp-vectorize，显示关闭相当于关闭 那俩；更多地使用浮点寄存器，是-O2到-O3负优化的罪魁祸首
-# -fno-pcc-struct-return -freg-struct-return :二进制不兼容
-# -ffinite-loops : clang中可能删除无线循环
-
-cat > /bin/gcc-me << EOF
-#!/bin/bash
-gcc \\
-    -fstack-reuse=all -fjump-tables -fbit-tests -fno-non-call-exceptions -fno-float-store -ftrivial-auto-var-init=uninitialized -fstdarg-opt -fno-conserve-stack -fzero-call-used-regs=skip -ffp-contract=fast \\
-    -fomit-frame-pointer -fstrict-aliasing -fdelete-null-pointer-checks -foptimize-sibling-calls -ftree-loop-vectorize -ftree-slp-vectorize -ffast-math -fno-rounding-math -fexcess-precision=fast -fno-signed-zeros -fno-trapping-math -fallow-store-data-races \\
-    -fdelete-dead-exceptions -fipa-pta -fdevirtualize-at-ltrans -fmerge-all-constants -funroll-loops -funroll-all-loops \\
-    -fno-exceptions -fno-asynchronous-unwind-tables -fno-unwind-tables \\
-    -fstack-check=no -fno-stack-clash-protection -fno-stack-protector -fno-split-stack -fcf-protection=none -fno-sanitize=all -fno-instrument-functions \\
-    -std=gnu2x -D_GNU_SOURCE -g0 -Ofast -Wall -Wextra -Wstrict-prototypes -Wshadow "\$@"
-EOF
-
-cat > /bin/g++-me << EOF
-#!/bin/bash
-g++ \\
-    -fstack-reuse=all -fjump-tables -fbit-tests -fno-non-call-exceptions -fno-float-store -ftrivial-auto-var-init=uninitialized -fstdarg-opt -fno-conserve-stack -fzero-call-used-regs=skip -ffp-contract=fast \\
-    -fomit-frame-pointer -fstrict-aliasing -fdelete-null-pointer-checks -foptimize-sibling-calls -ftree-loop-vectorize -ftree-slp-vectorize -ffast-math -fno-rounding-math -fexcess-precision=fast -fno-signed-zeros -fno-trapping-math -fallow-store-data-races \\
-    -fdelete-dead-exceptions -fipa-pta -fdevirtualize-at-ltrans -fmerge-all-constants -funroll-loops -funroll-all-loops \\
-    -fstack-check=no -fno-stack-clash-protection -fno-stack-protector -fno-split-stack -fcf-protection=none -fno-sanitize=all -fno-instrument-functions -fvtable-verify=none -fno-threadsafe-statics \\
-    -std=gnu++23 -D_GNU_SOURCE -g0 -Ofast -Wall -Wextra -Wshadow "\$@"
-EOF
-
-chmod +x /bin/gcc-me /bin/g++-me
-
-# clang 不支持的选项：
-# -fstack-reuse=all -fbit-tests -fno-float-store -fstdarg-opt -fno-conserve-stack
-# -fexcess-precision=fast -fallow-store-data-races
-# -fdelete-dead-exceptions -fipa-pta -fdevirtualize-at-ltrans -funroll-all-loops
-# -fno-instrument-functions
-# 和GCC不同的选项：
-# -ffinite-loops
-# -fno-rounding-math -ffp-contract=fast
-# -fslp-vectorize == -ftree-slp-vectorize
-# -fvectorize == -ftree-loop-vectorize
-
-cat > /usr/bin/clang-me << EOF
-#!/bin/bash
-clang${llvm_version} \\
-    -Xclang -pic-level -Xclang 0 -fno-addrsig -fuse-ld=lld-17 --rtlib=compiler-rt -unwindlib=libunwind \\
-    -fno-float-store -fexcess-precision=fast -funroll-all-loops -Wno-ignored-optimization-argument -Wno-unused-command-line-argument \\
-    -fjump-tables -fno-non-call-exceptions -ftrivial-auto-var-init=uninitialized -fzero-call-used-regs=skip -fno-rounding-math \\
-    -fomit-frame-pointer -fstrict-aliasing -fdelete-null-pointer-checks -foptimize-sibling-calls -fvectorize -fslp-vectorize -ffast-math -ffp-contract=fast -fno-signed-zeros -fno-trapping-math -funroll-loops \\
-    -fmerge-all-constants \\
-    -fno-exceptions -fno-asynchronous-unwind-tables -fno-unwind-tables \\
-    -fno-stack-check -fno-stack-clash-protection -fno-stack-protector -fno-split-stack -fcf-protection=none -fno-sanitize=all \\
-    -std=gnu2x -D_GNU_SOURCE -g0 -Ofast -Wall -Wextra -Wstrict-prototypes -Wshadow "\$@"
-EOF
-
-cat > /usr/bin/clang++-me << EOF
-#!/bin/bash
-clang++${llvm_version} \\
-    -Xclang -pic-level -Xclang 0 -fno-addrsig -fuse-ld=lld-17 --rtlib=compiler-rt -unwindlib=libunwind -stdlib=libc++ \\
-    -fno-float-store -fexcess-precision=fast -funroll-all-loops -Wno-ignored-optimization-argument -Wno-unused-command-line-argument \\
-    -fjump-tables -fno-non-call-exceptions -ftrivial-auto-var-init=uninitialized -fzero-call-used-regs=skip -fno-rounding-math \\
-    -fomit-frame-pointer -fstrict-aliasing -fdelete-null-pointer-checks -foptimize-sibling-calls -fvectorize -fslp-vectorize -ffast-math -ffp-contract=fast -fno-signed-zeros -fno-trapping-math -funroll-loops \\
-    -fmerge-all-constants \\
-    -fno-exceptions -fno-asynchronous-unwind-tables -fno-unwind-tables \\
-    -fno-stack-check -fno-stack-clash-protection -fno-stack-protector -fno-split-stack -fcf-protection=none -fno-sanitize=all \\
-    -std=gnu++2b -D_GNU_SOURCE -g0 -Ofast -Wall -Wextra -Wstrict-prototypes -Wshadow "\$@"
-EOF
-
-chmod +x /bin/clang-me /bin/clang++-me
-
-cat > /bin/aarch64-gcc-me << EOF
-#!/bin/bash
-aarch64-linux-gnu-gcc \\
-    -fstack-reuse=all -fjump-tables -fbit-tests -fno-non-call-exceptions -fno-float-store -ftrivial-auto-var-init=uninitialized -fstdarg-opt -fno-conserve-stack -fzero-call-used-regs=skip -ffp-contract=fast \\
-    -fomit-frame-pointer -fstrict-aliasing -fdelete-null-pointer-checks -foptimize-sibling-calls -ftree-loop-vectorize -ftree-slp-vectorize -ffast-math -fno-rounding-math -fexcess-precision=fast -fno-signed-zeros -fno-trapping-math -fallow-store-data-races \\
-    -fdelete-dead-exceptions -fipa-pta -fdevirtualize-at-ltrans -fmerge-all-constants -funroll-loops -funroll-all-loops \\
-    -fno-exceptions -fno-asynchronous-unwind-tables -fno-unwind-tables \\
-    -fstack-check=no -fno-stack-clash-protection -fno-stack-protector -fno-split-stack -fcf-protection=none -fno-sanitize=all -fno-instrument-functions \\
-    -std=gnu2x -D_GNU_SOURCE -g0 -Ofast -Wall -Wextra -Wstrict-prototypes -Wshadow "\$@"
-EOF
-
-cat > /bin/aarch64-g++-me << EOF
-#!/bin/bash
-aarch64-linux-gnu-g++ \\
-    -fstack-reuse=all -fjump-tables -fbit-tests -fno-non-call-exceptions -fno-float-store -ftrivial-auto-var-init=uninitialized -fstdarg-opt -fno-conserve-stack -fzero-call-used-regs=skip -ffp-contract=fast \\
-    -fomit-frame-pointer -fstrict-aliasing -fdelete-null-pointer-checks -foptimize-sibling-calls -ftree-loop-vectorize -ftree-slp-vectorize -ffast-math -fno-rounding-math -fexcess-precision=fast -fno-signed-zeros -fno-trapping-math -fallow-store-data-races \\
-    -fdelete-dead-exceptions -fipa-pta -fdevirtualize-at-ltrans -fmerge-all-constants -funroll-loops -funroll-all-loops \\
-    -fstack-check=no -fno-stack-clash-protection -fno-stack-protector -fno-split-stack -fcf-protection=none -fno-sanitize=all -fno-instrument-functions -fvtable-verify=none -fno-threadsafe-statics \\
-    -std=gnu++23 -D_GNU_SOURCE -g0 -Ofast -Wall -Wextra -Wshadow "\$@"
-EOF
-
-chmod +x /bin/aarch64-gcc-me /bin/aarch64-g++-me
-
-cat > /usr/bin/aarch64-clang-me << EOF
-#!/bin/bash
-clang${llvm_version} \\
-    --target=aarch64-linux-gnu \\
-    -Xclang -pic-level -Xclang 0 -fno-addrsig -fuse-ld=lld-17 --rtlib=compiler-rt \\
-    -fno-float-store -fexcess-precision=fast -funroll-all-loops -Wno-ignored-optimization-argument -Wno-unused-command-line-argument \\
-    -fjump-tables -fno-non-call-exceptions -ftrivial-auto-var-init=uninitialized -fzero-call-used-regs=skip -fno-rounding-math \\
-    -fomit-frame-pointer -fstrict-aliasing -fdelete-null-pointer-checks -foptimize-sibling-calls -fvectorize -fslp-vectorize -ffast-math -ffp-contract=fast -fno-signed-zeros -fno-trapping-math -funroll-loops \\
-    -fmerge-all-constants \\
-    -fno-exceptions -fno-asynchronous-unwind-tables -fno-unwind-tables \\
-    -fno-stack-check -fno-stack-clash-protection -fno-stack-protector -fno-split-stack -fcf-protection=none -fno-sanitize=all \\
-    -std=gnu2x -D_GNU_SOURCE -g0 -Ofast -Wall -Wextra -Wstrict-prototypes -Wshadow "\$@"
-EOF
-
-cat > /usr/bin/aarch64-clang++-me << EOF
-#!/bin/bash
-clang++${llvm_version} \\
-    --target=aarch64-linux-gnu \\
-    -Xclang -pic-level -Xclang 0 -fno-addrsig -fuse-ld=lld-17 --rtlib=compiler-rt \\
-    -fno-float-store -fexcess-precision=fast -funroll-all-loops -Wno-ignored-optimization-argument -Wno-unused-command-line-argument \\
-    -fjump-tables -fno-non-call-exceptions -ftrivial-auto-var-init=uninitialized -fzero-call-used-regs=skip -fno-rounding-math \\
-    -fomit-frame-pointer -fstrict-aliasing -fdelete-null-pointer-checks -foptimize-sibling-calls -fvectorize -fslp-vectorize -ffast-math -ffp-contract=fast -fno-signed-zeros -fno-trapping-math -funroll-loops \\
-    -fmerge-all-constants \\
-    -fno-exceptions -fno-asynchronous-unwind-tables -fno-unwind-tables \\
-    -fno-stack-check -fno-stack-clash-protection -fno-stack-protector -fno-split-stack -fcf-protection=none -fno-sanitize=all \\
-    -std=gnu++2b -D_GNU_SOURCE -g0 -Ofast -Wall -Wextra -Wstrict-prototypes -Wshadow "\$@"
-EOF
-
-chmod +x /bin/aarch64-clang-me /bin/aarch64-clang++-me
-
-cat >> /usr/share/vim/vim90/defaults.vim <<EOF
+cat >> /usr/share/vim/vim91/defaults.vim <<EOF
 
 set mouse-=a
 "set expandtab
-set tabstop=4
-set shiftwidth=4
-set softtabstop=4
+set tabstop=8
+set shiftwidth=8
+set softtabstop=8
 set autoindent
 set smartindent
 set cindent
 set cinoptions+=g0
 set hlsearch
-EOF
-
-cat >> ~/.inputrc << EOF
-"\e[5~": history-search-backward
-"\e[6~": history-search-forward
-"\e[H": beginning-of-line
-"\e[F": end-of-line
+"set number
 EOF
 
 sed -i '/^HISTSIZE=/d' ~/.bashrc
 sed -i '/^HISTFILESIZE=/d' ~/.bashrc
 sed -i '/^HISTTIMEFORMAT=/d' ~/.bashrc
 cat >> ~/.bashrc << EOF
+
 HISTSIZE=-1
 HISTFILESIZE=-1
 HISTTIMEFORMAT='%F %T '
+EOF
+
+cat >> ~/.inputrc << EOF
+
+"\e[5~": history-search-backward
+"\e[6~": history-search-forward
+"\e[H": beginning-of-line
+"\e[F": end-of-line
 EOF
 
 
